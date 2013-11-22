c5d01cc5330eaa44a71c37173ad84169c4e66498
joeylin <joe.y.lin.2012@gmail.com>
2013-11-22 11:25:58 +0800

    add test suite

--- test/color_test.js
+++ test/color_test.js
@@ -23,99 +23,85 @@
     module('color', {
         // This will run before each test in this module.
         setup: function() {
-          this.colorForm = {
-            rgb: 'rgb(0,0,0)',
-            rgba: 'rgba(0,0,0,1)',
-            hex: '#000',
-            hsl: 'hsl(0,100%, 50%)',
-          };
+            this.color = $.colorValue;
+            this.colorForm = {
+                rgb: 'rgb(0,0,0)',
+                rgba: 'rgba(0,0,0,1)',
+                hex: '#000',
+                hsl: 'hsl(0,100%, 50%)',
+            };
         }
-    });
-      
-
-    test('color-hex ',function() {
-        var color = new $.colorValue(this.hex);
-
-        console.log(this.hex);
-
-        console.log(color.get());
-
-        console.log(color.toRGBA()); //rgba(0,0,0,1)
-
-        console.log(color.toRGB()); //rgb(0,0,0)
-
-        console.log(color.toHSLA()); //hsla(0,0%,0%,1)
-
-        console.log(color.toHEX()); //#000000
-
-        console.log(color.toString()); //#000000
-
-
-
-        equal(
-          color.toRGBA(), 
-          'rgba(0,0,0,1)', 
-          "RGBA convert success"
-          );
-
-        equal(
-          color.toRGB(), 
-          'rgb(0,0,0)', 
-          "RGB convert success"
-          );
-
-        equal(
-          color.toHSLA(), 
-          'hsla(0,0%,0%,1)', 
-          "HSLA convert success"
-          );
-
-        equal(
-          color.toHEX(), 
-          '#000000', 
-          "RGBA convert success"
-          );
-
-        equal(
-          color.toString(), 
-          '#000000', 
-          "RGBA convert success"
-          );    
     });
 
     test('color-rgb ',function() {
-        var color = new $.colorValue(this.rgb);
+        var color = new this.color(this.colorForm.rgb);
+        equal(
+            color.toRGBA(), 
+            'rgba(0,0,0,1)', 
+            "RGBA convert success"
+        );
+        equal(
+            color.toRGB(), 
+            'rgb(0,0,0)', 
+            "RGB convert success"
+        );
+        equal(
+            color.toHSLA(), 
+            'hsla(0,0%,0%,1)', 
+            "HSLA convert success"
+        );
+        equal(
+            color.toHEX(), 
+            '#000000', 
+            "RGBA convert success"
+        );
+        equal(
+            color.toString(), 
+            'rgb(0,0,0)', 
+            "output string success"
+        );
+    });
+
+    test('color hex ',function() {
+        
+        var color = new this.color(this.colorForm.hex);
 
         equal(
-          color.toRGBA(), 
-          'rgba(0,0,0,1)', 
-          "RGBA convert success"
+            color.toRGBA(), 
+            'rgba(0,0,0,1)', 
+            "RGBA convert success"
         );
 
         equal(
-          color.toRGB(), 
-          'rgb(0,0,0)', 
-          "RGB convert success"
+            color.toRGB(), 
+            'rgb(0,0,0)', 
+            "RGB convert success"
         );
 
         equal(
-          color.toHSLA(), 
-          'hsla(0,0%,0%,1)', 
-          "HSLA convert success"
+            color.toHSLA(), 
+            'hsla(0,0%,0%,1)', 
+            "HSLA convert success"
         );
 
         equal(
-          color.toHEX(), 
-          '#000000', 
-          "RGBA convert success"
+            color.toHEX(), 
+            '#000000', 
+            "RGBA convert success"
         );
 
         equal(
-          color.toString(), 
-          '#000000', 
-          "RGBA convert success"
-        );
- 
+            color.toString(), 
+            '#000000', 
+            "RGBA convert success"
+        );    
+    });
+
+    test('other color', function() {
+        var colorHex = new this.color('#78783a');
+        equal(colorHex.toRGB(),'rgb(120,120,58)','change to RGB ok');
+        equal(colorHex.toRGBA(),'rgba(120,120,58,1)','change to RGBA ok');
+        equal(colorHex.toString(),'#78783a','change to string ok');
     });
 
   }(jQuery));
